ba540cb3605867a99c8614b241340ef4d5f1fd3b
jiangjun0206 <jacksweat93@outlook.com>
2019-03-26 11:17:26 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "kms0206";
+// MySQL Database Password
+$sql_db_pass = "kms0206";
+// MySQL Database Name
+$sql_db_name = "wowonder";
+
+// Site URL
+$site_url = "http://wowonderexample.com/"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "06d2b9ae-ce1a-45b5-ac4b-f4fe42135016"; // Your purchase code, don't give it to anyone. 
+?>
